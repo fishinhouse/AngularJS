f8ff027768381e05f4626746dab0f0c9b35cca05
wangliang <wangliang@rongyitech.com>
2015-07-04 16:16:15 +0800

    学习controller

---
+++ app/js/controller.js
@@ -0,0 +1,25 @@
+﻿'use strict'
+
+var angularApp=angular.module('angularApp',[]);
+
+angularApp.controller('LowPriceCarTypeCtrl', function($scope){
+$scope.items=[{
+		'type':'Toyota Rav4',
+		'city':'美国 - 洛杉矶',
+		'price':'166元/天'
+	},{
+		'type':'Mitsubishi Asx',
+		'city':'澳大利亚 - 霍巴特',
+		'price':'153元/天'
+	},{
+		'type':'Nissan Qashqai',
+		'city':'法国 - 巴黎',
+		'price':'171元/天'
+	},{
+		'type':'VOLKSWAGEN UP',
+		'city':'意大利 - 罗马',
+		'price':'177元/天'
+	}];
+});
+
+
